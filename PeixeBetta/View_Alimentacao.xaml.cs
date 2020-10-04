eba4ea7c5a56248f6cc2c7d123bd3e08b309dc17
hesau hugo <hesau@forsell.com.br>
2020-10-04 07:34:26 -0300

    commit inicial

---
+++ PeixeBetta/View_Alimentacao.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PeixeBetta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View_Alimentacao : ContentPage
+    {
+        public View_Alimentacao()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View_Alimentacao_AlimentosVivos());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View_Alimentacao_Racao());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View_Alimentacao_Jejum());
+        }
+    }
+}
---
+++ PeixeBetta/View_AquariosParametros.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PeixeBetta
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class View_AquariosParametros : ContentPage
+    {
+        public View_AquariosParametros()
+        {
+            InitializeComponent();
+        }
+    }
+}
